6a1bc5733148e6205eeba638a1751a38aa9f0d35
jamarke123 <repository@classify.com.ph>
2023-07-16 14:39:19 +0800

    dasdasdasdasdasd

--- routes/web.js
+++ routes/web.js
@@ -1,6 +1,13 @@
 const express = require("express");
 const route = express();
 
+route.get("/", async (req, res) => {
+  return res.status(201).json({
+    success: true,
+    message: "Welcome from capston system.",
+  });
+});
+
 route.use("/sms", require("../controllers/sms_controller"));
 
 module.exports = route;
