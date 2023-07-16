9849ceeb5a3b6987613f25ba9b7e5ff1cd2bb5ea
jamarke123 <repository@classify.com.ph>
2023-07-16 14:34:57 +0800

    asdasdasd

--- controllers/sms_controller.js
+++ controllers/sms_controller.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = express.Router();
 
 const accountSid = "AC398322c423ce0706f74e7db1ae4fb730";
-const authToken = "070e42ec4e7a56b163728b0e9e214888";
+const authToken = "7ccdcefe0ab757f0a21103ece51bfa54";
 const client = require("twilio")(accountSid, authToken);
 
 router.get("/", async (req, res) => {
@@ -20,26 +20,21 @@ router.post("/", async (req, res) => {
   if (!from || !to || !body)
     return res.status(404).json({ success: false, message: "Required inputs" });
 
-  return res.json({ success: true, message: "testststst" });
-  // try {
-  //   client.messages
-  //     .create({
-  //       body,
-  //       from,
-  //       to,
-  //     })
-  //     .then((message) => console.log(message.sid))
-  //     .done();
-  //   return res
-  //     .status(201) // response 201 means Created
-  //     .json({
-  //       success: true,
-  //       message: "successfully send",
-  //     });
-  // } catch (err) {
-  //   console.log(err);
-  //   return res.status(500).json(err);
-  // }
+  try {
+    await client.messages.create({
+      body,
+      from,
+      to,
+    });
+
+    return res.status(201).json({
+      success: true,
+      message: "Successfully sent",
+    });
+  } catch (err) {
+    console.log(err);
+    return res.status(500).json(err);
+  }
 });
 
 module.exports = router;
